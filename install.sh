78cd9dc80f5bc30f9a29f07fe2d53860995317e2
AzerixYT <realazerix@gmail.com>
2026-09-04 16:01:01 +0100

    Add no-new-privileges flag to Docker run command
    Updated Docker run command to include no-new-privileges flag.

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@ docker build . -t codespacesdesktop
 mkdir Save
 cp -r root/config/* Save
 
-docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=CodespacesDesktop -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped codespacesdesktop:latest
+# Updated below with the explicit false security override flag
+docker run -d --name=DesktopOnCodespaces \
+  -e PUID=1000 -e PGID=1000 \
+  --security-opt seccomp=unconfined \
+  --security-opt no-new-privileges=false \
+  -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=CodespacesDesktop \
+  -p 3000:3000 --shm-size="2gb" \
+  -v $(pwd)/Save:/config --restart unless-stopped codespacesdesktop:latest
 
 echo "INSTALL FINISHED! Check Port Tab"
